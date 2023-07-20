7e7d8ca3ad79ef7332c5547a5319704112658972
kavetech2023 <you@example.com>
2023-07-20 10:57:23 -0700

    zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
+ * Main - tests function that prints if interger is positive or negative
+ * Return
  */
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
 
-	else
-		return (0);
-}
+int main(void)
+
+	
+	
+{
+
+	int i;
+	i=0;
+	positive_or_negative(i);
+
+	return(0);
+}	
