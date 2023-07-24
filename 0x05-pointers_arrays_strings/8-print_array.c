1a00198d645b3f39fd87a9849f56c256952acb52
kavetech2023 <you@example.com>
2023-07-24 05:41:42 -0700

    eight

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,10 @@
 void print_array(int *a, int n)
 {
 	int index;
-	
-	for (index = 0; index < n; index++)
-	{
+	i
+	for (index = 0; index < n; index++){
 		printf("%d", a[index]);
-		if (index != (n - 1))
-		{
+		if (index != (n - 1)){
 			printf(", ");
 		}
 	}
