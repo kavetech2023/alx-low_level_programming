d0eeb01d0fedcbdc0a652737dc15c488bd9913de
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-02-23 22:19:29 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, to
+ * _puts - Function that prints a string, followed by a new line, to
  * stdout.
  *
- * @str: This is my input string
+ * @str: Input string
  *
  */
 
